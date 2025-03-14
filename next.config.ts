98d56a7eda1fae09d04a92c8bf6c60afa3f5a3e3
AldSenior <aldsio196@gmail.com>
2025-03-14 22:23:40 +0300

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	eslint: {
+		ignoreDuringBuilds: true, // Игнорировать ESLint во время сборки
+	},
+}
 
-export default nextConfig;
+export default nextConfig
