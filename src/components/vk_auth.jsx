507c78da357e1f76bc48f7e7e445fafe08693090
AldSenior <aldsio196@gmail.com>
2025-03-16 01:24:42 +0300

    c

--- src/components/vk_auth.jsx
+++ src/components/vk_auth.jsx
@@ -6,7 +6,7 @@ const VK_AUTH = () => {
 
 	useEffect(() => {
 		const script = document.createElement('script')
-		script.src = 'https://unpkg.com/@vkid/sdk@3.0.0/dist-sdk/umd/index.js'
+		script.src = 'https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js'
 		script.async = true
 		script.onload = () => {
 			if ('VKIDSDK' in window) {
