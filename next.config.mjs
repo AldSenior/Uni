d01717d78b991f53b4b0ea84d79563300273b92b
AldSenior <aldsio196@gmail.com>
2025-03-17 20:59:28 +0300

    e

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	async headers() {
@@ -6,7 +5,6 @@ const nextConfig = {
 			{
 				source: '/:path*',
 				headers: [
-					// CSP для VK ID
 					{
 						key: 'Content-Security-Policy',
 						value: [
@@ -14,24 +12,40 @@ const nextConfig = {
 							"default-src 'self'",
 
 							// Для VK ID SDK
-							"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://id.vk.com https://vk.com",
+							"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://id.vk.com https://login.vk.com",
 							"style-src 'self' 'unsafe-inline' https://id.vk.com",
-							"connect-src 'self' https://id.vk.com https://api.vk.com",
-							'frame-src https://id.vk.com',
+							"connect-src 'self' https://id.vk.com https://api.vk.com https://login.vk.com",
+							'frame-src https://id.vk.com https://login.vk.com https://www.unimessage.ru/vk-callback',
 							"img-src 'self' data: https://*.vk.com",
+
+							// Для Next.js
+							"font-src 'self' data:",
+							"manifest-src 'self'",
 						].join('; '),
 					},
 					// CORS headers
 					{
 						key: 'Access-Control-Allow-Origin',
-						value: 'https://www.unimessage.ru',
+						value: [
+							'https://id.vk.com',
+							'https://login.vk.com',
+							'https://www.unimessage.ru',
+						]
+							.filter(Boolean)
+							.join(' '),
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET, POST, PUT, DELETE, OPTIONS',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'Content-Type, Authorization',
 					},
 				],
 			},
 		]
 	},
-	// Отключаем предзагрузку шрифтов
-	optimizeFonts: false,
 }
 
 export default nextConfig
