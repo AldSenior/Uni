b17c59f65aeb9ca8bc1b601706ea3f7e2a194d7c
AldSenior <aldsio196@gmail.com>
2025-03-17 20:17:40 +0300

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-// next.config.js
-const securityHeaders = [
-	{
-		key: 'Content-Security-Policy',
-		value:
-			"frame-ancestors 'self' https://vk.com https://*.vk.com https://vk.ru https://*.vk.ru https://web.vk.me https://*.pages-ac.vk-apps.com;",
-	},
-]
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = {
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: securityHeaders,
-			},
-		]
-	},
-}
+export default nextConfig
