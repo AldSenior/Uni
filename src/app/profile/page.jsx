4beb0663e0e8a9dcd66d6c1200f1ae582d086283
AldSenior <aldsio196@gmail.com>
2025-03-18 20:48:45 +0300

    v2(

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,37 +1,55 @@
+'use client'
+
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-async function getUserData() {
-	const accessToken = window.localStorage.getItem('vkAccessToken')
-	if (!accessToken) {
-		return
-	}
+export default function Profile() {
+	const [user, setUser] = useState(null)
+	const [loading, setLoading] = useState(true)
 
-	try {
-		const response = await axios.get('https://api.vk.com/method/users.get', {
-			params: {
-				access_token: accessToken,
-				v: '5.131', // Версия API VK
-			},
-		})
-		return response.data.response[0]
-	} catch (error) {
-		console.error('Ошибка при получении данных пользователя:', error)
-	}
-}
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const token = localStorage.getItem('vkAccessToken')
 
-function Profile({ user }) {
+				if (!token) {
+					window.location.href = '/login'
+					return
+				}
+
+				const response = await axios.get(
+					'https://api.vk.com/method/users.get',
+					{
+						params: {
+							access_token: token,
+							v: '5.131',
+							fields: 'photo_200,first_name,last_name',
+						},
+					}
+				)
+
+				setUser(response.data.response[0])
+			} catch (error) {
+				console.error('Ошибка загрузки:', error)
+				localStorage.removeItem('vkAccessToken')
+				window.location.href = '/login'
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		fetchUser()
+	}, [])
+
+	if (loading) return <div>Loading...</div>
+
 	return (
 		<div>
-			<h1>Добро пожаловать, {user.first_name}!</h1>
-			<p>Ваш профиль:</p>
+			<h1>
+				{user.first_name} {user.last_name}
+			</h1>
+			<img src={user.photo_200} alt='Avatar' />
 			<pre>{JSON.stringify(user, null, 2)}</pre>
 		</div>
 	)
 }
-
-export async function getServerSideProps() {
-	const user = await getUserData()
-	return { props: { user } }
-}
-
-export default Profile
