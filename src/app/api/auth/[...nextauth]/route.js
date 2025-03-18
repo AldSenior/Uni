173e88fc57fce33622c673f2d45119ccd0c5988b
AldSenior <aldsio196@gmail.com>
2025-03-18 21:18:57 +0300

    v2(

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 // app/api/auth/[...nextauth]/route.js
-import { auth, handlers, signIn, signOut } from '@/auth'
+import { auth, handlers, signIn, signOut } from '../../../auth'
 
 export const { GET, POST } = handlers
 export { auth, signIn, signOut }
