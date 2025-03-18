f130a63ee91c1352d4fcaed64a17debe21bf8e5c
AldSenior <aldsio196@gmail.com>
2025-03-18 21:02:48 +0300

    v2(

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,36 +1,5 @@
-import NextAuth from 'next-auth'
-import VKProvider from 'next-auth/providers/vk'
+// app/api/auth/[...nextauth]/route.js
+import { auth, handlers, signIn, signOut } from '@/auth'
 
-export const {
-	handlers: { GET, POST },
-	auth,
-	signIn,
-	signOut,
-} = NextAuth({
-	providers: [
-		VKProvider({
-			clientId: 53263292,
-			clientSecret: 'xK4loxyZGbRjhC7OjBw2',
-			authorization: {
-				params: {
-					scope: 'email',
-					display: 'popup',
-					v: '5.131',
-				},
-			},
-		}),
-	],
-	secret: process.env.NEXTAUTH_SECRET,
-	callbacks: {
-		async jwt({ token, account }) {
-			if (account) {
-				token.accessToken = account.access_token
-			}
-			return token
-		},
-		async session({ session, token }) {
-			session.accessToken = token.accessToken
-			return session
-		},
-	},
-})
+export const { GET, POST } = handlers
+export { auth, signIn, signOut }
