c0d1018afd90b6c1790066fb498af95761a27d71
AldSenior <aldsio196@gmail.com>
2025-04-07 13:12:35 +0300

    ??

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.mjs
+import { withContentSecurityPolicy } from "next-content-security-policy";
 
-export default nextConfig
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://trustedscripts.example.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  frame-ancestors 'self' https://vk.com https://*.vk.com;
+`.replace(/\n/g, "");
+
+export default withContentSecurityPolicy({
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // применяем для всех маршрутов
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: contentSecurityPolicy,
+          },
+        ],
+      },
+    ];
+  },
+});
