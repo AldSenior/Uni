8868a59763104f748f302e20f1f0e87ed0f4f705
AldSenior <aldsio196@gmail.com>
2025-04-07 13:03:34 +0300

    ??

--- src/app/vk-callback/page.jsx
+++ src/app/vk-callback/page.jsx
@@ -1,9 +1,9 @@
-import VK_AUTH from '@/components/vk_auth'
+import VK_AUTH from "../../app/components/vk_auth";
 export default function vkPageAuth() {
-	return (
-		<>
-			<p>Проверка</p>
-			<VK_AUTH />
-		</>
-	)
+  return (
+    <>
+      <p>Проверка</p>
+      <VK_AUTH />
+    </>
+  );
 }
