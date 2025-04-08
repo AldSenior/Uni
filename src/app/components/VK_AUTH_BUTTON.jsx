3367b34dcb3607ce9278b6eec737ed0e1eb79348
AldSenior <aldsio196@gmail.com>
2025-04-08 10:31:51 +0300

    __

--- src/app/components/VK_AUTH_BUTTON.jsx
+++ src/app/components/VK_AUTH_BUTTON.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
-import { useVKAuth } from "../hooks/useVKAuth";
-
+import { useVKAuth } from "../../hooks/useVKAuth";
+//
 export default function VKAuthButton({ onSuccess, onError }) {
   const { startAuth, exchangeCodeForToken, isLoading, error } = useVKAuth();
 
